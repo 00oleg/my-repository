4d38816556d783ba000e365a4a26f49789aa59ac
Oleg Bendiucov <punisher8585@gmail.com>
2024-07-07 14:08:25 +0300

    feat: add loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import ErrorBoundary from './components/ErrorBoundary';
 import SearchPage from './pages/Search';
 
 function App() {
-  return <SearchPage params={{}} />;
+  return (
+    <ErrorBoundary fallback={<p>ErrorBoundary: Something went wrong.</p>}>
+      <SearchPage params={{}} />
+    </ErrorBoundary>
+  );
 }
 
 export default App;
